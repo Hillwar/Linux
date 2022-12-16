3838cec2e77e6de6feece11f2ac466e9eb3f9438
kirill <7ndowswi@gmail.com>
2022-12-16 20:23:32 +0300

    Mod 4

--- lab4/lab4.sh
+++ lab4/lab4.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-#1
+echo 1
 dnf groupinstall "Development Tools"
 
-#2
+echo 2
 tar -xzvf bastet-0.43.tgz
-sudo yum install boost-devel
-sudo yum install ncurses-devel
+sudo yum install boost-devel 2>>/dev/null
+sudo yum install ncurses-devel 2>>/dev/null
 cd bastet-0.43
 make
 echo "install:[\n TAB] cp ~/lab4/bastet/bastet /usr/bin [\n TAB] chmod 775 /usr/bin/bastet" >> Makefile
 make install
 
-#3
+echo 3
 dnf list installed > ~/task3.log
 
-#4
+echo 4
 dnf deplist gcc > ~/task4_1.log
 rpm -q --whatrequires libgcc > ~/task4_2.log
 
-#5
-yum install createrepo
+echo 5
+yum install createrepo 2>>/dev/null
 mkdir localrepo
 cd localrepo
 cp checkinstall-1.6.2-3.el6.1.x86_64.rpm ~/localrepo/checkinstall-1.6.2-3.el6.1.x86_64.rpm
@@ -29,10 +29,10 @@ cd /etc/yum.repos.d
 touch localrepo.repo
 echo -e "[localrepo]\nname=localrepo\nbaseurl=file:///root/localrepo/\nenabled=1\ngpgcheck=0" > localrepo.repo
 
-#6
+echo 6
 dnf repolist all > ~/task6.log
 
-#7
+echo 7
 cd /etc/yum.repos.d/
 for f in *; do
 	mv "$f" "$(echo "$f" | sed s/repo/oldrepos/)";
@@ -41,16 +41,16 @@ mv localrepo.oldrepos localrepo.repo
 dnf list available
 dnf install checkinstall.x86_64
 
-#8
+echo 8
 cp fortunes-ru_1.52-2_all.deb ~/fortunes-ru_1.52-2_all.deb
-yum install alien
+yum install alien 2>>/dev/null
 alien --to-rpm ~/fortunes-ru_1.52-2_all.deb
 rpm -i ~/fortunes-ru-1.52-3.noarch.rpm
 
-#9
-dnf download nano
-dnf install https://extras.getpagespeed.com/release-el8-latest.rpm
-dnf install rpmrebuild
+echo 9
+dnf download nano 2>>/dev/null
+dnf install https://extras.getpagespeed.com/release-el8-latest.rpm 2>>/dev/null
+dnf install rpmrebuild 2>>/dev/null
 
 rpmrebuild -enp nano-2.9.8-1.el8.x86_64.rpm
 

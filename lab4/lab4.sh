129ec5546732e8df07e9b7a0d5eaa296d6a61f61
kirill <7ndowswi@gmail.com>
2022-12-16 21:40:36 +0300

    Mod 4

--- lab4/lab4.sh
+++ lab4/lab4.sh
@@ -11,7 +11,7 @@ sudo yum install boost-devel
 sudo yum install ncurses-devel 
 cd bastet-0.43
 make
-echo "install:\n\tinstall ./bastet /usr/bin" > Makefile
+echo "install:[\n TAB] cp ./bastet /usr/bin [\n TAB] chmod 775 /usr/bin/bastet" > Makefile
 make install
 
 read -p "Введите полное имя: " fullname
